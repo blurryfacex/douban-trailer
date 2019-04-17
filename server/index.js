05e7538a29b9ffb2b325d41352594e56364d9666
blurryfacex <306965405@qq.com>
2019-04-17 18:03:32 +0800

    create server somehing

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const Koa = require('koa')
 const app = new Koa()
+const views = require('koa-views')
+const {resolve} = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 
 app.use(async (ctx, next) => {
-    ctx.body = 'movie index'
+  await ctx.render('index', {
+    me: 'luke',
+    you: 'Scoot',
+  })
 })
 
 app.listen(4555)
